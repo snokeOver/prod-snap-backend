7a70b71cb2dcec8637bbdc456b775065d7b68b89
snokeOver <snokeover@gmail.com>
2024-08-17 19:58:02 +0600

    Updated the getFeaturedMobile api

--- dbOperations/getMethods/getFeaturedMobile.js
+++ dbOperations/getMethods/getFeaturedMobile.js
@@ -5,10 +5,10 @@ export const getFeaturedMobile = async (req, res, next) => {
   try {
     const response = await MobileModel.find({})
       .sort({ createdAt: -1 }) // Sort by `createdAt` field in descending order
-      .limit(6); // Limit the result to six documents
+      .limit(8); // Limit the result to six documents
 
     res.status(200).send({
-      message: "Recent 6 mobile data retrieved successfully!",
+      message: "Featured 6 mobile data retrieved successfully!",
       response,
     });
   } catch (err) {
